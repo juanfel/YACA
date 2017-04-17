184ecc54a6be1ee376a4b7be11a46993fd6429b8
juanfel <jf3dsaber@gmail.com>
2017-04-17 19:30:08 -0300

    Completa el adaptador de listas de receta.

--- app/src/main/java/com/juanfel/yaca/RecipeListAdapter.java
+++ app/src/main/java/com/juanfel/yaca/RecipeListAdapter.java
@@ -1,32 +1,62 @@
 package com.juanfel.yaca;
 
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 /**
  * Created by juanfel on 17-04-17.
  */
 
 public class RecipeListAdapter extends BaseExpandableListAdapter {
+    private final List<Recipe> recipes;
+    public android.view.LayoutInflater inflater;
+    public AppCompatActivity activity;
+
+    public RecipeListAdapter(AppCompatActivity act, List<Recipe> recipes){
+        this.recipes = recipes;
+        this.activity = act;
+        inflater = this.activity.getLayoutInflater();
+    }
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null){
+            convertView = inflater.inflate(R.layout.detail_recipe_list, null);
+        }
+        String detail = (String) getChild(groupPosition, childPosition);
+        TextView text = (TextView) convertView.findViewById(R.id.recipe_list_content_text);
+        text.setText(detail);
+        return convertView;
     }
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.header_recipe_list, null);
+        }
+        Recipe recipe = (Recipe) getGroup(groupPosition);
+        TextView text = (TextView) convertView.findViewById(R.id.list_recipe_title);
+        text.setText(recipe.nombre);
+
+        return convertView;
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return recipes.get(groupPosition).children.get(childPosition);
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return recipes.get(groupPosition);
     }
 
     @Override
@@ -36,17 +66,17 @@ public class RecipeListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return recipes.size();
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return recipes.get(groupPosition).id;
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return recipes.get(groupPosition).children.size();
     }
 
     @Override
