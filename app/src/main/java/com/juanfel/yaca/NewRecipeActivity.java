97253e553521d2e156e59dd245eb2af3fb8d38fa
juanfel <jf3dsaber@gmail.com>
2017-07-07 02:40:23 -0400

    Crea numberpickers para hacer recetas.

--- app/src/main/java/com/juanfel/yaca/NewRecipeActivity.java
+++ app/src/main/java/com/juanfel/yaca/NewRecipeActivity.java
@@ -1,11 +1,11 @@
 package com.juanfel.yaca;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 /**
@@ -19,6 +19,14 @@ public class NewRecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_recipe);
 
         final TextView titleView = (TextView)findViewById(R.id.new_recipe_title);
+
+        final NumberPicker timer = (NumberPicker)findViewById(R.id.new_recipe_time);
+        timer.setMinValue(0);
+
+        final NumberPicker ratio = (NumberPicker)findViewById(R.id.new_recipe_ratio);
+        ratio.setMinValue(0);
+        ratio.setMaxValue(100); //Usualmente los ratio están entre 1:4 a 1:20, siendo el mas usual 1:16
+        ratio.setValue(16);
 
         Button saveButton = (Button)findViewById(R.id.new_recipe_save_button);
 
