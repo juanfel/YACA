6feb98ab35ffb482e58268ff355ee2c5d1a65e7a
juanfel <jf3dsaber@gmail.com>
2017-07-07 02:47:30 -0400

    Cambia la clase del contenido de la receta para que sea consistente con el resto del codigo.

--- app/src/main/java/com/juanfel/yaca/RecipeStep.java
+++ app/src/main/java/com/juanfel/yaca/RecipeStep.java
@@ -1,22 +1,21 @@
 package com.juanfel.yaca;
 
+
 /**
  * Created by juanfel on 24-05-17.
  * Controla las variables que están involucradas en cada paso.
  */
 
-public class Recipe_Step {
-    float step_time; //En segundos
+public class RecipeStep {
+    int step_time; //En segundos
     Boolean has_timer;
-    float water_percent;
-    float coffee_percent;
+    int coffee_percent;
 
-    public Recipe_Step(Boolean has_timer, float step_time, float water_percent, float coffee_percent){
+    public RecipeStep(Boolean has_timer, int step_time,int coffee_percent){
         if(has_timer){
             this.has_timer = has_timer;
             this.step_time = step_time;
         }
-        this.water_percent = water_percent;
         this.coffee_percent = coffee_percent;
     }
 }
